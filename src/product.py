94e42705bdeeead420816c3dc3d54bc13bbb1821
Nikodim26 <lr1975monk@gmail.com>
2026-08-17 19:00:13 +0400

    изменил аннотацию

--- src/product.py
+++ src/product.py
@@ -9,7 +9,7 @@ class Product:
         Product.product_count += 1
 
     @classmethod
-    def new_product(cls, dict_product: dict) -> Product:
+    def new_product(cls, dict_product: dict) -> 'Product':
         return Product(**dict_product)
 
     @property
